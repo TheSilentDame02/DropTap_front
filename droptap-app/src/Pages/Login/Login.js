7db85e405369d8267ea87f3516a7ea3eab16bbaa
OugaMehdi <95387941+OugaMehdi@users.noreply.github.com>
2022-11-26 18:09:36 +0100

    login page added and updated

--- droptap-app/src/Pages/Login/Login.js
+++ droptap-app/src/Pages/Login/Login.js
@@ -1,8 +1,9 @@
-import { Box, VStack, HStack, Heading, FormControl, Button} from '@chakra-ui/react'
+import { Box, Center, VStack, HStack, Heading, FormControl, Button} from '@chakra-ui/react'
 import { Input } from '@chakra-ui/react'
 import { InputGroup, InputRightElement } from '@chakra-ui/react';
 import { BiShowAlt, BiHide } from 'react-icons/bi';
 import { ReactComponent as Logo } from '../../Assets/Images/logo.svg';
+import '../../Assets/Styles/Login.css';
 
 
 import React from "react";
@@ -11,21 +12,29 @@ export default function Login(){
     const [show, setShow] = React.useState(false)
     const handleClick = () => setShow(!show)
     return (
+        <Center h='100vh'>
+        <HStack spacing={0} >
         <Box 
             w={['full', 'sm']} 
-            p={[8, 10]} 
-            mt={[20, '10vh']} 
-            mx='auto' 
-            border={['none', '1px']} 
-            borderColor={['', 'gray.300']} 
-            borderRadius={10}
+            h='400px'
+            p={[8, 10]}
+            // borderLeft='1px' 
+            // borderTop='1px'
+            // borderBottom='1px'
+            boxShadow='2xl' 
+            rounded='md' 
+            bg='white'
+            borderColor={['', 'gray.300']}
+            borderLeftRadius={10}
+            borderRightRadius={0}
+            backgroundColor='rgba(255,255,255,0.7)'
         >
         <VStack spacing={4} w='full'>
             <VStack spacing={1} align={['flex-start','center']} w='full'>
             <Heading w='40'><Logo/></Heading>
             </VStack>
         <FormControl>
-            <Input type='email' variant='filled' placeholder='Login' mt='10' borderRadius={17} required/>
+            <Input type='email' variant='filled' placeholder='Login' mt='4' borderRadius={17} required/>
             <InputGroup size='md'>
                 <Input pr='4.5rem' type={show ? 'text' : 'password'} placeholder='Mot de passe' variant='filled' mt='5' borderRadius={17} required/>
                 <InputRightElement w='4.5rem' mt='5'>
@@ -45,5 +54,10 @@ export default function Login(){
         </Button>
         </VStack>
         </Box>
+        <Box id="bg" borderRightRadius={10}>
+            <div style={{height:'400px', width:'200px'}} ></div>
+        </Box>
+        </HStack>
+        </Center>
     );
 }
