952a2928d28767ea46763a4ea88d6635255def92
TheSilentDame02 <larroussi.sara@gmail.com>
2022-12-09 23:56:37 +0000

    adding profile info,modal for seuil de consommation  and aligning settings with notification

--- droptap-app/src/Pages/Parametres.js
+++ droptap-app/src/Pages/Parametres.js
@@ -1,18 +1,43 @@
-import { Box, Grid, GridItem ,Table,Th,Td,Tr,Switch,Heading,Stack, HStack, VStack} from '@chakra-ui/react'
+import { Box, Grid, GridItem ,Table,Td,Tr,Switch,Heading,Button,Modal,ModalOverlay,ModalContent,ModalHeader,ModalFooter,ModalBody,ModalCloseButton,useDisclosure, FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+Input,Text} from '@chakra-ui/react'
 import Notifications from "../Components/Notifications";
+import { CiEdit } from "react-icons/ci";
+import { IoTrendingUp } from 'react-icons/io5';
+import '../Assets/Styles/Settings.css';
+
 
 const Settings = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     return ( 
         <>
-        <Grid templateColumns='repeat(8, 1fr)' templateRows='repeat(20, 1fr)' gap={4} >
+        <br></br>
+        <Grid templateColumns='repeat(9, 1fr)' templateRows='repeat(20, 1fr)' gap={4} >
             <GridItem colSpan={6} rowSpan={1}>
                 <Heading as="h1" size="md" fontWeight="600">
                     Parametres
                 </Heading>
             </GridItem>
+            <GridItem colSpan={3} rowSpan={1}>
+            <Heading as="h1" size="md" fontWeight="600">
+                    Notifications
+                </Heading>
+            </GridItem>
             <GridItem colSpan={6} rowSpan={19}>
                 
                 <Box w='100%' p={8} bg="white" borderRadius="20px" >
+                    <div className='profile'>
+                    <Heading as="h3" size="sm" fontWeight="600">
+                    Nom Prenom/Code du produit
+                </Heading>
+                <Text>E-mail</Text>
+                <div >
+                <Button borderRadius={"20px"} bg="trasparent"><CiEdit /></Button>
+                </div>
+                
+                    </div>
                     <Table>
                     <Heading as="h1" size="md" fontWeight="600">
                     Parametres technique
@@ -28,7 +53,29 @@ const Settings = () => {
                         </Tr>
                         <Tr>
                             <Td>Seuil de consommation</Td>
-                            <Td>200L</Td>
+                            <Td>200L <Button bg="transparent" onClick={onOpen}><CiEdit /></Button>
+                            <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose} closeOnOverlayClick={false} >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Seuil de Consommation</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+          <FormControl>
+  <FormLabel>Seuil de consommation</FormLabel>
+  <Input type='text' placeholder='200' />
+  <FormHelperText>Veuillez entrez la consommation du mois</FormHelperText>
+</FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Discard
+            </Button>
+            <Button variant='ghost'>Save</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+                            </Td>
                         </Tr>
                         <br/>
                         <Heading as="h1" size="md" fontWeight="600">Parametres de Notifications</Heading>
@@ -59,12 +106,8 @@ const Settings = () => {
                     </Table>
                 </Box>
             </GridItem>
-            <GridItem colSpan={2} rowSpan={1}>
-            <Heading as="h1" size="md" fontWeight="600">
-                    Notifications
-                </Heading>
-            </GridItem>
-            <GridItem colSpan={2} rowSpan={19}>
+            
+            <GridItem colSpan={3} rowSpan={19}>
                 <Notifications/>
             </GridItem>
         </Grid>
