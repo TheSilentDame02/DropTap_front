c2dfd3001beecca67a6b36fb37e02743201d519f
W5oAmI <ohassan864@gmail.com>
2022-12-16 00:10:45 +0100

    debit rectifier

--- droptap-app/src/Pages/Rapport.js
+++ droptap-app/src/Pages/Rapport.js
@@ -1,5 +1,5 @@
 import Notifications from '../Components/Notifications';
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import { Box, Heading, Flex, HStack,Tabs, TabList, Tab, TabPanel, TabPanels, Center, VStack,Text, Stack, Progress } from '@chakra-ui/react';
 import BarChart from '../Components/BarChart'
 import { UserData } from '../Components/DayData';
@@ -7,6 +7,20 @@ import LineChart from '../Components/LineChart';
 import { BsDropletHalf } from 'react-icons/bs';
 import { extendTheme } from "@chakra-ui/react";
 import {FaMoneyBillWave} from 'react-icons/fa'
+import Api from "../services/api";
+
+
+let dataStatistique;
+
+Api.getConsommations().then(response => {
+    dataStatistique = response.data;
+});
+
+let dataSeuil;
+Api.getSeuilByType("jour").then(response => {
+    dataSeuil = response.data.valeur
+});
+
 
 export default function Rapport(){
 
@@ -18,8 +32,13 @@ export default function Rapport(){
     }
 });
 
-    
-    const [userData,setUserData] = useState({
+    const [prixConsommationDay, setPrixConsommationDay] = useState(0);
+    const [prixConsommationMonth, setPrixConsommationMonth] = useState(0);
+    const [prixConsommationYear, setPrixConsommationYear] = useState(0);
+    const [seuilJour, setSeuilJour] = useState(0);
+
+
+    const [userDataDay,setUserDataDay] = useState({
         labels: UserData.map((data) => data.hour),
         datasets: [{
             label: "Consommation d'eau",
@@ -30,6 +49,149 @@ export default function Rapport(){
             barPercentage: 0.5,
         }]
     });
+
+    const [userDataMonth,setUserDataMonth] = useState({
+        labels: UserData.map((data) => data.hour),
+        datasets: [{
+            label: "Consommation d'eau",
+            data: UserData.map((data) => data.hourCons),
+            borderRadius: 1999,
+            backgroundColor: '#55C2FF',
+            barThickness: 10,
+            barPercentage: 0.5,
+        }]
+    });
+
+    const [userDataYear,setUserDataYear] = useState({
+        labels: UserData.map((data) => data.hour),
+        datasets: [{
+            label: "Consommation d'eau",
+            data: UserData.map((data) => data.hourCons),
+            borderRadius: 1999,
+            backgroundColor: '#55C2FF',
+            barThickness: 10,
+            barPercentage: 0.5,
+        }]
+    });
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSeuilJour(dataSeuil);
+
+            let statistiqueConsommationDay =[
+                ['00', 0], ['01', 0], ['02', 0], ['03', 0], ['04', 0], ['05', 0],
+                ['06', 0], ['07', 0], ['08', 0], ['09', 0], ['10', 0], ['11', 0],
+                ['12', 0], ['13', 0], ['14', 0], ['15', 0], ['16', 0], ['17', 0],
+                ['18', 0], ['19', 0], ['20', 0], ['21', 0], ['22', 0], ['23', 0]];
+
+
+            let statistiqueConsommationMonth =[
+                ['01', 0], ['02', 0], ['03', 0], ['04', 0], ['05', 0],
+                ['06', 0], ['07', 0], ['08', 0], ['09', 0], ['10', 0], ['11', 0],
+                ['12', 0], ['13', 0], ['14', 0], ['15', 0], ['16', 0], ['17', 0],
+                ['18', 0], ['19', 0], ['20', 0], ['21', 0], ['22', 0], ['23', 0],
+                ['24', 0], ['25', 0], ['26', 0], ['27', 0], ['28', 0], ['29', 0],
+                ['30', 0],['31', 0]]
+
+
+
+            let statistiqueConsommationYear=[
+                ['01', 0], ['02', 0], ['03', 0], ['04', 0],
+                ['05', 0], ['06', 0], ['07', 0], ['08', 0],
+                ['09', 0], ['10', 0], ['11', 0], ['12', 0],
+            ]
+
+            let statistiquePrixDay = 0;
+
+            let statistiquePrixMonth = 0;
+
+            let statistiquePrixYear = 0;
+
+            let jourStatistique = ((new Date()).toString().split(' '))[2];
+
+            dataStatistique.forEach(data => {
+                let jourConsommation = (((data.createdAt.toString().split('T'))[0]).split('-'));
+
+                if (jourConsommation[2]==jourStatistique){
+                    for (let i=0; i<24; i++){
+                        if ((statistiqueConsommationDay[i][0])==(((data.createdAt.toString().split('T'))[1]).split(':'))[0]){
+                            statistiqueConsommationDay[i][1] += data.debit;
+                        }
+                    }
+                }
+
+                for (let j=0; j<31; j++){
+                    if (jourConsommation[2] == statistiqueConsommationMonth[j][0]){
+                        statistiqueConsommationMonth[j][1] += data.debit;
+                    }
+                }
+
+
+
+                for (let j=0; j<12; j++){
+                    if (jourConsommation[1] == statistiqueConsommationYear[j][0]){
+                        statistiqueConsommationYear[j][1] += data.debit;
+                    }
+                }
+
+            });
+
+            for (let j=0; j<24; j++){
+                statistiquePrixDay += statistiqueConsommationDay[j][1];
+            }
+
+            for (let j=0;j<31;j++){
+                statistiquePrixMonth += statistiqueConsommationMonth[j][1];
+            }
+
+            for (let j=0; j<12; j++){
+                statistiquePrixYear += statistiqueConsommationYear[j][1];
+            }
+
+            setPrixConsommationDay(statistiquePrixDay);
+            setPrixConsommationMonth(statistiquePrixMonth);
+            setPrixConsommationYear(statistiquePrixYear);
+
+            setUserDataDay({
+                labels: statistiqueConsommationDay.map((data)=>data[0]),
+                datasets: [{
+                    label: "Consommation d'eau",
+                    data: statistiqueConsommationDay.map((data)=>Math.floor(data[1])),
+                    borderRadius: 1999,
+                    backgroundColor: '#55C2FF',
+                    barThickness: 10,
+                    barPercentage: 0.5,
+
+                }]
+            });
+
+            setUserDataMonth({
+                labels: statistiqueConsommationMonth.map((data)=>data[0]),
+                datasets: [{
+                    label: "Consommation d'eau",
+                    data: statistiqueConsommationMonth.map((data)=>Math.floor(data[1])),
+                    borderRadius: 1999,
+                    backgroundColor: '#55C2FF',
+                    barThickness: 10,
+                    barPercentage: 0.5,
+
+                }]
+            });
+
+            setUserDataYear({
+                labels: statistiqueConsommationYear.map((data)=>data[0]),
+                datasets: [{
+                    label: "Consommation d'eau",
+                    data: statistiqueConsommationYear.map((data)=>Math.floor(data[1])),
+                    borderRadius: 1999,
+                    backgroundColor: '#55C2FF',
+                    barThickness: 10,
+                    barPercentage: 0.5,
+
+                }]
+            });
+        }, 1000);
+    }, []);
     
     return(
         <>
@@ -53,42 +215,79 @@ export default function Rapport(){
                         <Tabs variant='soft-rounded' colorScheme='blue'>
                             <TabList bgColor={'#F3F3F3'} width='-webkit-fit-content' m='auto' borderRadius={9999}>
                                 <Tab>Day</Tab>
-                                <Tab>Week</Tab>
                                 <Tab>Month</Tab>
+                                <Tab>Year</Tab>
                             </TabList>
                             <TabPanels>
                                 <TabPanel>
                                 <Center mb={2} fontSize='large'>Statistics of the Day: </Center>
-                                <BarChart chartData={userData} />
+                                <BarChart chartData={userDataDay} />
+                                    <Center>
+                                        <VStack>
+                                            <HStack>
+                                                <HStack>
+                                                    <BsDropletHalf style={{color: "#20a4f4",fontSize: "2em"}}/>
+                                                    <Text fontWeight='bolder'>{seuilJour }L</Text>
+                                                </HStack>
+                                                <Text fontSize={12} fontWeight='bold' pl={'40px'} >{(prixConsommationDay / (seuilJour ) ).toFixed(2)}% of </Text>
+                                            </HStack>
+                                            <Progress value={(prixConsommationDay / (seuilJour ) ).toFixed(2)} colorScheme='twitter' width='full' borderRadius={20} />
+                                            <Text pt={5} fontSize={18} fontWeight='bold' >Estimated water bill: </Text>
+                                            <HStack>
+                                                <FaMoneyBillWave style={{color: "green",fontSize: "2em"}}/>
+                                                <Text fontWeight='bolder'>{prixConsommationDay/250}</Text>
+                                            </HStack>
+                                        </VStack>
+
+                                    </Center>
                                 </TabPanel>
                                 <TabPanel>
                                 <Center mb={2} fontSize='large'>Statistics of the Week: </Center>
-                                <BarChart chartData={userData} />
+                                <BarChart chartData={userDataMonth} />
+                                    <Center>
+                                        <VStack>
+                                            <HStack>
+                                                <HStack>
+                                                    <BsDropletHalf style={{color: "#20a4f4",fontSize: "2em"}}/>
+                                                    <Text fontWeight='bolder'>{seuilJour * 31}L</Text>
+                                                </HStack>
+                                                <Text fontSize={12} fontWeight='bold' pl={'40px'} >{(prixConsommationMonth / (seuilJour * 31)).toFixed(2) }% of </Text>
+                                            </HStack>
+                                            <Progress value={(prixConsommationMonth / (seuilJour * 31)).toFixed(2) } colorScheme='twitter' width='full' borderRadius={20} />
+                                            <Text pt={5} fontSize={18} fontWeight='bold' >Estimated water bill: </Text>
+                                            <HStack>
+                                                <FaMoneyBillWave style={{color: "green",fontSize: "2em"}}/>
+                                                <Text fontWeight='bolder'>{prixConsommationMonth/250}</Text>
+                                            </HStack>
+                                        </VStack>
+
+                                    </Center>
                                 </TabPanel>
                                 <TabPanel>
                                 <Center mb={2} fontSize='large'>Statistics of the Month:</Center>
-                                <BarChart chartData={userData} />
+                                <BarChart chartData={userDataYear} />
+                                    <Center>
+                                        <VStack>
+                                            <HStack>
+                                                <HStack>
+                                                    <BsDropletHalf style={{color: "#20a4f4",fontSize: "2em"}}/>
+                                                    <Text fontWeight='bolder'>{seuilJour * 31 * 12}L</Text>
+                                                </HStack>
+                                                <Text fontSize={12} fontWeight='bold' pl={'40px'} > {(prixConsommationYear / (seuilJour * 31 * 12)).toFixed(2)  }% of </Text>
+                                            </HStack>
+                                            <Progress value={(prixConsommationYear / (seuilJour * 31 * 12)).toFixed(2)  } colorScheme='twitter' width='full' borderRadius={20} />
+                                            <Text pt={5} fontSize={18} fontWeight='bold' >Estimated water bill: </Text>
+                                            <HStack>
+                                                <FaMoneyBillWave style={{color: "green",fontSize: "2em"}}/>
+                                                <Text fontWeight='bolder'>{prixConsommationYear/250}</Text>
+                                            </HStack>
+                                        </VStack>
+
+                                    </Center>
                                 </TabPanel>
                             </TabPanels>
                         </Tabs>
-                        <Center>
-                            <VStack>
-                                <HStack>
-                                <HStack>
-                                    <BsDropletHalf style={{color: "#20a4f4",fontSize: "2em"}}/>
-                                    <Text fontWeight='bolder'>80L</Text>
-                                </HStack>
-                                <Text fontSize={12} fontWeight='bold' pl={'40px'} >70% of </Text>
-                                </HStack>
-                                <Progress value={70} colorScheme='twitter' width='full' borderRadius={20} />
-                                <Text pt={5} fontSize={18} fontWeight='bold' >Estimated water bill: </Text>
-                                <HStack>
-                                    <FaMoneyBillWave style={{color: "green",fontSize: "2em"}}/>
-                                    <Text fontWeight='bolder'>200DH</Text>
-                                </HStack>
-                            </VStack>
-                            
-                        </Center>
+
                     </Box>
                 </Flex>
             </Box>
